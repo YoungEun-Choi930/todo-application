d0487e721332aa6f383368a889aadc6b2096d079
YoungEun-Choi930 <duddms0190@naver.com>
2021-11-05 16:26:09 +0900

    sql DB, load method

---
+++ app/src/main/java/com/example/todo/SQLiteDBHelper.java
@@ -0,0 +1,108 @@
+package com.example.todo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteDBHelper extends SQLiteOpenHelper {
+    public SQLiteDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        String subject = "CREATE TABLE IF NOT EXISTS SubjectList" +
+                "(subjectName TEXT PRIMARY KEY, number INT, startWeekNumber INT, startTime INT, endWeekNumber INT, endTime INT);";
+        String lecture = "CREATE TABLE IF NOT EXISTS LectureList" +
+                "(subjectName TEXT, lectureName TEXT, startDate INT, endDate INT, isDone INT, " +
+                "FOREIGN KEY(subjectName) REFERENCES subjet(subjectName));";
+        String exam = "CREATE TABLE IF NOT EXISTS ExamList(subjectName TEXT, date INT, " +
+                "FOREIGN KEY(subjectName) REFERENCES subjet(subjectName));";
+        String assingment = "CREATE TABLE IF NOT EXISTS AssingmentList" +
+                "(subjectName TEXT, assingmentName TEXT, startDate INT, endDate INT, isDone INT, " +
+                "FOREIGN KEY(subjectName) REFERENCES subjet(subjectName));";
+        String friend = "CREATE TABLE IF NOT EXISTS Friends(friendID);";
+
+        DB.execSQL(subject);
+        DB.execSQL(lecture);
+        DB.execSQL(exam);
+        DB.execSQL(assingment);
+        DB.execSQL(friend);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("DROP TABLE IF EXISTS LectureList;");
+        onCreate(DB);
+    }
+
+    public Boolean excuteQuery(String query){
+        SQLiteDatabase DB = getWritableDatabase();
+        DB.execSQL(query);                          //에러 exception추가하고 결과값 반환으로 수정하기
+        return true;
+    }
+
+    public List<LectureInfo> loadLectureList(String date) {
+        SQLiteDatabase DB = getWritableDatabase();
+        String query = "SELECT subjectName, lectureName, isDone FROM LectureList WHERE startDate < "+date+" AND endDate > "+date+";";
+        Cursor cursor = DB.rawQuery(query,null);
+
+        List<LectureInfo> list = new ArrayList<>();
+        while(cursor.moveToNext()){
+            LectureInfo info = new LectureInfo();
+            info.setSubjectName(cursor.getString(0));
+            info.setLectureName(cursor.getString(1));
+            info.setIsDone(Boolean.valueOf(cursor.getString(2)));
+            list.add(info);
+        }
+        return list;
+    }
+    public List<ExamInfo> loadExamList(String date) {
+        SQLiteDatabase DB = getWritableDatabase();
+        String query = "SELECT subjectName, examName FROM ExamList WHERE startDate < "+date+" AND endDate > "+date+";";
+        Cursor cursor = DB.rawQuery(query,null);
+
+        List<ExamInfo> list = new ArrayList<>();
+        while(cursor.moveToNext()){
+            ExamInfo info = new ExamInfo();
+            info.setSubjectName(cursor.getString(0));
+            info.setExamName(cursor.getString(1));
+            list.add(info);
+        }
+        return list;
+    }
+    public List<AssingmentInfo> loadAssingmentList(String date) {
+        SQLiteDatabase DB = getWritableDatabase();
+        String query = "SELECT subjectName, assingmentName, isDone FROM AssingmentList WHERE startDate < "+date+" AND endDate > "+date+";";
+        Cursor cursor = DB.rawQuery(query,null);
+
+        List<AssingmentInfo> list = new ArrayList<>();
+        while(cursor.moveToNext()){
+            AssingmentInfo info = new AssingmentInfo();
+            info.setSubjectName(cursor.getString(0));
+            info.setAssingmentName(cursor.getString(1));
+            info.setIsDone(Boolean.valueOf(cursor.getString(2)));
+            list.add(info);
+        }
+        return list;
+    }
+    public List<SubjectInfo> loadSubjectList() {
+        SQLiteDatabase DB = getWritableDatabase();
+        String query = "SELECT subjectName FROM SubjectList;";
+        Cursor cursor = DB.rawQuery(query,null);
+
+        List<SubjectInfo> list = new ArrayList<>();
+        while(cursor.moveToNext()){
+            SubjectInfo info = new SubjectInfo();
+            info.setSubjectName(cursor.getString(0));
+            list.add(info);
+        }
+        return list;
+    }
+}
