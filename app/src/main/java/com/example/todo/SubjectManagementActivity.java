ed6065bcd703a4a57917073d02e34025823f2d39
YoungEun-Choi930 <69569809+YoungEun-Choi930@users.noreply.github.com>
2021-11-06 14:55:39 +0900

    sqlite수정

--- app/src/main/java/com/example/todo/SubjectManagementActivity.java
+++ app/src/main/java/com/example/todo/SubjectManagementActivity.java
@@ -8,22 +8,33 @@ import java.util.List;
 
 public class SubjectManagementActivity extends AppCompatActivity {
 
-    SQLiteDBHelper sqlitehelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_management);
     }
     public List<SubjectInfo> getSubjectList() {
-        return sqlitehelper.loadSubjectList();
+        SQLiteDBAdapter adapter = new SQLiteDBAdapter(getApplicationContext());
+        adapter.open();
+        List<SubjectInfo> list = adapter.loadSubjectList();
+        adapter.close();
+        return list;
     }
-    public void addSubject(String subjectName, int number, int startWeekNumber, String startTime, int endWeekNumber, String endTime){
+    public boolean addSubject(String subjectName, int number, int startWeekNumber, String startTime, int endWeekNumber, String endTime){
         String query = "INSERT INTO SubjectList VALUES('"+
                 subjectName+"',"+number+","+startWeekNumber+",'"+startTime+"',"+endWeekNumber+",'"+endTime+"');";
-        boolean result = sqlitehelper.excuteQuery(query); //성공 또는 실패 값 들고올랬는데 아직 구현 안함
+        SQLiteDBAdapter adapter = new SQLiteDBAdapter(getApplicationContext());
+        adapter.open();
+        boolean result = adapter.excuteQuery(query);
+        adapter.close();
+        return result;
     }
-    public void delSubject(String subjectName) {
+    public boolean delSubject(String subjectName) {
         String query = "DELETE FROM SubjectList WHERE subjectName = '"+subjectName+"';";
-        boolean result = sqlitehelper.excuteQuery(query);
+        SQLiteDBAdapter adapter = new SQLiteDBAdapter(getApplicationContext());
+        adapter.open();
+        boolean result = adapter.excuteQuery(query);
+        adapter.close();
+        return result;
     }
 }
