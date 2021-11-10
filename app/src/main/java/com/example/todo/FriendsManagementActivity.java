8bb466898d28120923425aa214145103cae4aaab
YoungEun-Choi930 <duddms0190@naver.com>
2021-11-10 15:48:34 +0900

    friend management에 intent 추가

--- app/src/main/java/com/example/todo/FriendsManagementActivity.java
+++ app/src/main/java/com/example/todo/FriendsManagementActivity.java
@@ -2,7 +2,10 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.List;
 
 public class FriendsManagementActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class FriendsManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_management);
+
+        Intent getintent = getIntent();
     }
+    public List<String> getFriendsList_sqlite(){
+        SQLiteDBAdapter adapter = SQLiteDBAdapter.getInstance(getApplicationContext());
+        List<String> list = adapter.loadFriendsList();
+        return list;
+    }
+//    public List<String> getFriendsList_firebase(String userID){
+//
+//    }
+//    public List<String> getFriendsRequestList_firebase(){
+//
+//    }
+
+
 }
