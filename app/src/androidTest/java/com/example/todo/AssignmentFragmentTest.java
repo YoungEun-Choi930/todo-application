2e235255306ccf02af08401f5ad7de974d611e27
sws256 <77598142+sws256@users.noreply.github.com>
2021-12-03 22:06:55 +0900

    ㅋㅋ

--- app/src/androidTest/java/com/example/todo/AssignmentFragmentTest.java
+++ app/src/androidTest/java/com/example/todo/AssignmentFragmentTest.java
@@ -4,32 +4,31 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.doubleClick;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
+import java.util.List;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AssignmentFragmentTest {
@@ -95,50 +94,50 @@ public class AssignmentFragmentTest {
         ActivityScenario.launch(SubjectManagementActivity.class);
         ActivityScenario.launch(AddSubjectActivity.class);
 
-        onView(ViewMatchers.withId(R.id.name_subject)).perform(typeText("assignmentTest"));     //과목이름
-        onView(ViewMatchers.withId(R.id.semester2)).perform(doubleClick());         // 학기
-        onView(ViewMatchers.withId(R.id.number_subject)).perform(typeText("1"));       //강의갯수
-        onView(ViewMatchers.withId(R.id.start_4)).perform(doubleClick());           //시작요일
-        onView(ViewMatchers.withId(R.id.startTime_subject)).perform(click());       //시작시간
+        onView(withId(R.id.name_subject)).perform(typeText("assignmentTest"));     //과목이름
+        onView(withId(R.id.semester2)).perform(doubleClick());         // 학기
+        onView(withId(R.id.number_subject)).perform(typeText("1"));       //강의갯수
+        onView(withId(R.id.start_4)).perform(doubleClick());           //시작요일
+        onView(withId(R.id.startTime_subject)).perform(click());       //시작시간
         onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(10, 10));
         onView(ViewMatchers.withText("확인")).perform(click());
-        onView(ViewMatchers.withId(R.id.end_3)).perform(doubleClick());             // 종료요일
-        onView(ViewMatchers.withId(R.id.endTime_subject)).perform(click());         //종료시간
+        onView(withId(R.id.end_3)).perform(doubleClick());             // 종료요일
+        onView(withId(R.id.endTime_subject)).perform(click());         //종료시간
         onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(10, 10));
         onView(ViewMatchers.withText("확인")).perform(click());
-        onView(ViewMatchers.withId(R.id.year_subject)).perform(click());            //년도
-        onView(ViewMatchers.withId(R.id.btn_confirm)).perform(click());      //년도
+        onView(withId(R.id.year_subject)).perform(click());            //년도
+        onView(withId(R.id.btn_confirm)).perform(click());      //년도
 
 
-        onView(ViewMatchers.withId(R.id.yes_subject)).perform(click());             //확인버튼
+        onView(withId(R.id.yes_subject)).perform(click());             //확인버튼
         // 과목 test - 2021 2학기 - 강의1개 - 수~화 - 0000,0000 생성
 
     }
 
     private void insertAssignment(assignment a) {
-        onView(ViewMatchers.withId(R.id.assignment)).perform(click());          //과제버튼
-        onView(ViewMatchers.withId(R.id.et_todoname)).perform(typeText(a.assignmentName));  //과제이름
+        onView(withId(R.id.assignment)).perform(click());          //과제버튼
+        onView(withId(R.id.et_todoname)).perform(typeText(a.assignmentName));  //과제이름
 
-        onView(ViewMatchers.withId(R.id.startdate_a)).perform(click());         //시작날짜
+        onView(withId(R.id.startdate_a)).perform(click());         //시작날짜
         onView(ViewMatchers.withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(a.startYear,a.startMonth,a.startDay));
         onView(ViewMatchers.withText("확인")).perform(click());
 
-        onView(ViewMatchers.withId(R.id.starttime_a)).perform(click());         //시작시간
+        onView(withId(R.id.starttime_a)).perform(click());         //시작시간
         onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(a.startHour,a.startMinute));
         onView(ViewMatchers.withText("확인")).perform(click());
 
-        onView(ViewMatchers.withId(R.id.enddate_a)).perform(click());           //종료날짜
+        onView(withId(R.id.enddate_a)).perform(click());           //종료날짜
         onView(ViewMatchers.withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(a.endYear,a.endMonth,a.endDay));
         onView(ViewMatchers.withText("확인")).perform(click());
 
-        onView(ViewMatchers.withId(R.id.endtime_a)).perform(click());           //종료시간
+        onView(withId(R.id.endtime_a)).perform(click());           //종료시간
         onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(a.endHour,a.endMinute));
         onView(ViewMatchers.withText("확인")).perform(click());
 
-        onView(ViewMatchers.withId(R.id.btn_yes)).perform(click());             //확인버튼
+        onView(withId(R.id.btn_yes)).perform(click());             //확인버튼
     }
 
-    public boolean sqliteAssignmnetListTrue(assignment a){
+    public boolean sqliteAssignmentListTrue(assignment a){
         // 과제가 존재하고, 입력한 정보와 똑같이 들어갔으면 true, 과제가 존재하지 않으면 false, 정보가 다르면 error
         SQLiteDB db = SQLiteDB.getInstance();
 
@@ -179,7 +178,7 @@ public class AssignmentFragmentTest {
 
         assignment a = new assignment("test1","20210901","0000","20211001","0000");
         insertAssignment(a);
-        assertTrue(sqliteAssignmnetListTrue(a));
+        assertTrue(sqliteAssignmentListTrue(a));
     }
 
     // 과목추가와는 다른 피커라서 년도에 대한 피커 테스트만 함
@@ -199,7 +198,7 @@ public class AssignmentFragmentTest {
 
         a.startDate = 19000901;
         a.endDate = 19001001;
-        assertTrue(sqliteAssignmnetListTrue(a));
+        assertTrue(sqliteAssignmentListTrue(a));
     }
 
     @Test
@@ -216,7 +215,7 @@ public class AssignmentFragmentTest {
         assignment a = new assignment("test3","19000901","0000","19001001","0000");
         insertAssignment(a);
 
-        assertTrue(sqliteAssignmnetListTrue(a));
+        assertTrue(sqliteAssignmentListTrue(a));
     }
 
     @Test
@@ -233,7 +232,7 @@ public class AssignmentFragmentTest {
         assignment a = new assignment("test4","21000901","0000","21001001","0000");
         insertAssignment(a);
 
-        assertTrue(sqliteAssignmnetListTrue(a));
+        assertTrue(sqliteAssignmentListTrue(a));
     }
 
     @Test
@@ -252,7 +251,7 @@ public class AssignmentFragmentTest {
 
         a.startDate = 21000901;
         a.endDate = 21001001;
-        assertTrue(sqliteAssignmnetListTrue(a));
+        assertTrue(sqliteAssignmentListTrue(a));
     }
 
     @Test
@@ -269,7 +268,7 @@ public class AssignmentFragmentTest {
         assignment a = new assignment("test6","20211001","0000","20210901","0000");
         insertAssignment(a);
 
-        assertFalse(sqliteAssignmnetListTrue(a));
+        assertFalse(sqliteAssignmentListTrue(a));
     }
 
     @Test
@@ -286,19 +285,25 @@ public class AssignmentFragmentTest {
         assignment a = new assignment("test7","20210901","1200","20210901","1100");
         insertAssignment(a);
 
-        assertFalse(sqliteAssignmnetListTrue(a));
+        assertFalse(sqliteAssignmentListTrue(a));
+    }
+
+
+    @Test
+    public void 과제삭제(){
+
     }
 
 
     @AfterClass
     public static void deleteSubject() {        // Before Class에서 생성한 test과목 삭제
         ActivityScenario.launch(SubjectManagementActivity.class);
-        onView(ViewMatchers.withId(R.id.del_subject)).perform(click());
+        onView(withId(R.id.del_subject)).perform(click());
 
         SubjectInfo subjectInfo = new SubjectInfo("assignmentTest");
         SubjectManagementActivity.subjectAdapter.checkedList.add(subjectInfo);
 
-        onView(ViewMatchers.withId(R.id.btn_del_sub)).perform(click());
+        onView(withId(R.id.btn_del_sub)).perform(click());
     }
 
 
